3d770a3dd6b5285310252a2c45a70ff8b1952b73
Wontae-Lee <maxandjava@gmail.com>
2024-06-26 12:56:00 +0200

    changed the diagnosis options

--- src/validator.py
+++ src/validator.py
@@ -216,7 +216,7 @@ class Validator:
                     """)
             elif 'DSMC_MeanCollProb' in column:
                 collision_probability = np.max(dsmc_data.data[:, index])
-                if 0.1 < collision_probability:
+                if 0.1 < collision_probability < 1:
                     continue
                 else:
                     raise Exception(f"""
